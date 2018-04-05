e45d68edc00b17c373a372bf285594db00ddd7a6
tapas <mail.tapasranjanmishra@gmail.com>
2018-04-05 08:31:05 +0530

    Added library

---
+++ AveryConsoleApp/AveryBL/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AveryBL
+{
+    public class Utility
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

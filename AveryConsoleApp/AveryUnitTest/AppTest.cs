2b1592f28eb194d4b607d7d1b4ccb782b3f991dd
tapas <mail.tapasranjanmishra@gmail.com>
2018-04-05 10:38:29 +0530

    added Test Project

---
+++ AveryConsoleApp/AveryUnitTest/AppTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AveryBL;
+
+namespace AveryUnitTest
+{
+    [TestClass]
+    public class AppTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Utility util = new Utility();
+
+            Assert.AreEqual(11, util.Add(5, 6));
+
+        }
+    }
+}
